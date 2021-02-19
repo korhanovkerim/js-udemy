e1613692049ceb7e8661e369d45a87cb9b2f15a3
Kerimberdi <kerim.korhanov@mail.ru>
2021-02-19 21:03:46 +0600

    practice 2

--- js/script.js
+++ js/script.js
@@ -13,11 +13,24 @@ const personalMoviaDb = {
     privat: false
 };
 
-let lastMovie = prompt("один из последний фильм?", ''),
-     movieStar = prompt("На сколько оцените его?", ''),
-    lastMovieTwo = prompt("один из последний фильм?", ''),
-    movieStarTwo = prompt("На сколько оцените его?", '');
 
-    personalMoviaDb.movies[lastMovie] = movieStar;
-    personalMoviaDb.movies[lastMovieTwo] = movieStarTwo;
+
+    for(let i=0; i < 2; i++){
+        let a = prompt("один из последний фильм?", ''),
+            b = prompt("На сколько оцените его?", '');
+        if(a != null && b != null && a != "" && b != "" && a.length < 50){
+            personalMoviaDb.movies[a] = b;
+        }else{
+            i--;
+        }
+            
+    }
+if(personalMoviaDb.count < 10){
+    console.log("Просмотрена давно мало кино");
+} else if(personalMoviaDb.count >10 && personalMoviaDb.count < 30){
+    console.log("He is classical cinoman");
+} else{
+    console.log("He is cinoman");
+}
+
 console.log(personalMoviaDb);
