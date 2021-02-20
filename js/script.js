8bd58ffeecff9713d2dd14aea20865ca18f6166e
Kerimberdi <kerim.korhanov@mail.ru>
2021-02-20 13:30:29 +0600

    practice3

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 "use strict";
 
-let numberOfFilms = +prompt("Сколько фильмы вы уже смотрели?",'');
+let numberOfFilms;
 
+function start(){
+    numberOfFilms = +prompt("Сколько фильмы вы уже смотрели?",'');   
+
+    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = +prompt("Сколько фильмы вы уже смотрели?",'');    
+    }
+}
+
+// start();
 
 
 const personalMoviaDb = {
@@ -15,6 +24,7 @@ const personalMoviaDb = {
 
 
 
+function rememberMyFilms(){
     for(let i=0; i < 2; i++){
         let a = prompt("один из последний фильм?", ''),
             b = prompt("На сколько оцените его?", '');
@@ -25,12 +35,32 @@ const personalMoviaDb = {
         }
             
     }
-if(personalMoviaDb.count < 10){
-    console.log("Просмотрена давно мало кино");
-} else if(personalMoviaDb.count >10 && personalMoviaDb.count < 30){
-    console.log("He is classical cinoman");
-} else{
-    console.log("He is cinoman");
+}
+// rememberMyFilms();
+
+function detectPersonalLevel(){
+    if(personalMoviaDb.count < 10){
+        console.log("Просмотрена давно мало кино");
+    } else if(personalMoviaDb.count >10 && personalMoviaDb.count < 30){
+        console.log("He is classical cinoman");
+    } else{
+        console.log("He is cinoman");
+    }
 }
 
-console.log(personalMoviaDb);
+// detectPersonalLevel();
+
+function showMyDB(){
+    if(!personalMoviaDb.privat){
+        console.log(personalMoviaDb);
+    }
+}
+
+showMyDB();
+
+function writeYourGenres(){
+    for(let i = 1; i < 4; i++){
+        personalMoviaDb.genres[i] = prompt(`Ваш любимый жанр под номером ${i}`);
+    }
+}
+writeYourGenres();
